7a3d0143d874ad2a2fdede6cc7be04d619ce118f
Bakhtiyar2021 <bakhtiyaruddin@yahoo.com>
2021-10-14 00:31:04 -0400

    class 3

--- src/main/java/base/Practice.java
+++ src/main/java/base/Practice.java
@@ -1,13 +1,20 @@
 package base;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Practice {
     public static void main(String[] args){
 
-        String x = "test";
-        system.out.println(x);
-
-
-
-
+//        driver.get("http://qa.taltektc.com/home/login");
+//
+//
+//        driver.findElement(By.name("email")).sendKeys("hasanyc2@gmail.com");
+//        driver.findElement(By.name("password")).sendKeys("Test$1234");
+//        driver.findElement(By.xpath("//input[@value='Log In']")).click();
+//        try {
+//            assertEquals(driver.findElement(By.xpath("//form[@id='profile_form']/legend")).getText(), "Welcome to TalentTek");
+//        } catch (Error e) {
+//            verificationErrors.append(e.toString());
+//        }
     }
 }
---
+++ src/test/java/stepDef/login_step.java
@@ -0,0 +1,9 @@
+package stepDef;
+
+import io.cucumber.java.en.Given;
+
+public class login_step {
+    @Given("I am at TalentTEK Homepage")
+    public void iAmAtTalentTEKHomepage() {
+    }
+}
